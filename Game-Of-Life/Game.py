1f806865105b48855af2f4801c355505cdb0a5f6
Hayate84 <dddimitriou84@gmail.com>
2021-01-01 18:14:59 +0200

    Game_of_life.py documented. Start the refactor o pygame engine

--- Game-Of-Life/Game.py
+++ Game-Of-Life/Game.py
@@ -11,29 +11,36 @@ class Game(object):
     CELLS_HEIGHT = 60
 
     SIZE_OF_RECTANGLES = 10
+
+    BLACK = (255, 255, 255)
+    WHITE = (0, 0, 0)
+
+    LOGO_PATH       = "assets/images/logo32x32.png"
+    WINDOW_TITLE    = "Game of Life"
     
     def __init__(self):
 
         pygame.init()
 
-        self._set_logo("assets/images/logo32x32.png", "Game of Life")
+        self._set_window(self.LOGO_PATH, self.WINDOW_TITLE)
 
         self._screen = self._set_display(self.WIDTH, self.HEIGHT)
-        
-        self._screen.fill((0, 255, 0))
 
         self._running = True
 
+        self._clock = pygame.time.Clock()
+        self._FPS   = 3
+        
         self._rect_list = self._init_rectangle_list(self.WIDTH, self.HEIGHT, self.SIZE_OF_RECTANGLES)
 
         self._game_of_life = Game_of_life(self.CELLS_HEIGHT, self.CELLS_WIDTH)
 
-    def _set_logo(self, logo_path, caption_name):
+    def _set_window(self, logo_path, window_title):
 
         logo = pygame.image.load(logo_path)
 
         pygame.display.set_icon(logo)
-        pygame.display.set_caption(caption_name)
+        pygame.display.set_caption(window_title)
 
     def _set_display(self, width, height):
 
@@ -43,28 +50,32 @@ class Game(object):
 
         while self._running:
 
+            self._clock.tick(self._FPS)
+
             self._handleEvents()
 
-            for i in range(self.CELLS_HEIGHT):
-                for j in range(self.CELLS_WIDTH):
-
-                    current_rectangle = self._rect_list[i][j]
-                    
-                    if self._game_of_life._board[i][j]:
-
-                        pygame.draw.rect(self._screen, (0, 0, 0), current_rectangle)
-
-                    else:
-
-                        pygame.draw.rect(self._screen, (255, 255, 255), current_rectangle)
-                        
-            self._game_of_life.next_time_step()
-
-            pygame.time.delay(400)
-
+            self._Game_of_life_logic()
+         
             pygame.display.flip()
             
         pygame.quit()
+
+    def _Game_of_life_logic(self):
+
+        for i in range(self.CELLS_HEIGHT):
+            for j in range(self.CELLS_WIDTH):
+
+                current_rectangle = self._rect_list[i][j]
+                    
+                if self._game_of_life._board[i][j]:
+
+                    pygame.draw.rect(self._screen, self.WHITE, current_rectangle)
+
+                else:
+                        
+                    pygame.draw.rect(self._screen, self.BLACK, current_rectangle)
+                        
+        self._game_of_life.next_time_step()
 
     def _init_rectangle_list(self, sum_x, sum_y, size):
 
@@ -91,11 +102,23 @@ class Game(object):
 
         for event in pygame.event.get():
 
-            if event.type == pygame.QUIT:  self._running = False
-            if event.type == pygame.KEYUP:
-                
-                self._game_of_life = Game_of_life(self.CELLS_HEIGHT, self.CELLS_WIDTH)
+            if event.type == pygame.QUIT:       self._running = False
+            if event.type == pygame.KEYDOWN:    self._keyboardControls(event)
+                  
+    def _keyboardControls(self, event):
 
+        if event.key == pygame.K_r:
+            
+            self._game_of_life = Game_of_life(self.CELLS_HEIGHT, self.CELLS_WIDTH)
+            
+        elif event.key == pygame.K_q:
+            
+            self._running = False
+            
+        else:
+            
+            return
+        
 if __name__== "__main__":
 
     game = Game()
--- Game-Of-Life/Game_of_life.py
+++ Game-Of-Life/Game_of_life.py
@@ -2,30 +2,36 @@ from random import randint
 
 class Game_of_life(object):
 
+    """Class that implements the rules of Conway's Game of Life"""
+
     def __init__(self, rows, cols):
 
         self.ALIVE = 1
         self.DEAD  = 0
-
+        
+        #World is a 2x2 board with cell in position i, j 
         self._board = []
 
         self._rows  = rows
         self._cols  = cols
 
         for _ in range(rows):
-
+            
+            #create the initial cells a random 
             row = [randint(self.DEAD, self.ALIVE) for _ in range(cols)]
             self._board.append(row)
 
     def _get_neighbors(self, i, j):
         
+        """Count the neighbors of a cell in i, j position in the 2x2 board/world"""
+
         board = self._board
 
         rows = self._rows 
         cols = self._cols 
 
         total_neighbors = 0
-
+	
         if i + 1 < rows: total_neighbors += board[i + 1][j]
         if j + 1 < cols: total_neighbors += board[i][j + 1]
         
@@ -44,6 +50,8 @@ class Game_of_life(object):
         return self._board
 
     def next_time_step(self):
+
+        """Update all the cells based on the rules"""
 
         new_board = []
         old_board = self._board
@@ -65,6 +73,8 @@ class Game_of_life(object):
         self._board = new_board
                 
     def _rules(self, cell, neighbors):
+    
+        """Method to get the rules of the game based on if a cell is alive or dead"""
 
         if cell == self.ALIVE:
             return self._alive_rules(neighbors)
@@ -72,11 +82,15 @@ class Game_of_life(object):
             return self._dead_rules(neighbors)
               
     def _alive_rules(self, neighbors):
+	
+        """Rules in case a cell is alive"""
 
         if neighbors == 3 or neighbors == 2:    return self.ALIVE
         else:                                   return self.DEAD
 
     def _dead_rules(self, neighbors):
+
+        """Rules in case a cell is dead"""
 
         if neighbors == 3:  return self.ALIVE
         else:               return self.DEAD
